0499bba937249bca7d3e051a9c9ee507b545df53
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-06 12:09:25 +0530

    🔥 Modify code for our use

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,8 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+export interface Env {
+    test: string
+}
 declare namespace App {
-	// interface Error {}
-	// interface Locals {}
-	// interface PageData {}
-	// interface Platform {}
+    interface Platform {
+        env: Env
+    }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,36 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-	adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            routes: {
+                include: ["/*"],
+                exclude: ["<all>"]
+            }
+        }),
+        csp: {
+            mode: 'auto',
+            directives: {
+                'default-src': ['self'],
+                'img-src': ['self', 'data:', 'blob:'],
+                'object-src': ['none'],
+                'base-uri': ['self'],
+                'style-src': [
+                    'self', 'unsafe-inline'
+                ],
+                'style-src-elem': [
+                    'self', 'unsafe-inline',
+                    'sha256-KTPJY0ik6ufLv48oDKCYFYaptcCX75UrmWytfSjy+tA=',
+                    'sha256-8M+b2Hj+vy/2J5tZ9pYDHeuPD59KsaEZn1XXj3xVhjg='
+                ]
+            }
+        },
+        files: {
+            serviceWorker: 'src/service-worker.ts'
+        }
+    }
 };
 
 export default config;
