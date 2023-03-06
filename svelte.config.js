b2f8d377906f60e25818334aef2d82f46540537d
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-06 12:10:16 +0530

    🔥 Remove csp directives for now
    Too annoying

--- svelte.config.js
+++ svelte.config.js
@@ -13,21 +13,7 @@ const config = {
             }
         }),
         csp: {
-            mode: 'auto',
-            directives: {
-                'default-src': ['self'],
-                'img-src': ['self', 'data:', 'blob:'],
-                'object-src': ['none'],
-                'base-uri': ['self'],
-                'style-src': [
-                    'self', 'unsafe-inline'
-                ],
-                'style-src-elem': [
-                    'self', 'unsafe-inline',
-                    'sha256-KTPJY0ik6ufLv48oDKCYFYaptcCX75UrmWytfSjy+tA=',
-                    'sha256-8M+b2Hj+vy/2J5tZ9pYDHeuPD59KsaEZn1XXj3xVhjg='
-                ]
-            }
+            mode: 'auto'
         },
         files: {
             serviceWorker: 'src/service-worker.ts'
