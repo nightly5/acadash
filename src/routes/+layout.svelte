09b94eb0ac825da32a3cbaacd6fe7b05ea7219a1
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-08 22:49:46 +0530

    ✨ Functioning navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,31 @@
 <div class="app font-sans">
     <header>
         <nav class="navbar bg-neutral">
-            <div class="navbar-start">
-                <button class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+            <div class="navbar-start flex-1">
+                <a class="btn btn-ghost normal-case text-xl" href="/">
+                    Acadash
+                </a>
+            </div>
+            <div class="navbar-end hidden flex-1 lg:flex">
+                <ul class="menu menu-horizontal px-1">
+                    <li><a href="/test1">Test 1</a></li>
+                    <li><a href="/test2">Test 2</a></li>
+                </ul>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-ghost lg:hidden" type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"
+                        />
+                    </svg>
                 </button>
-            </div>
-            <div class="flex-1">
-              <a class="btn btn-ghost normal-case text-xl" href="/">Acadash</a>
-            </div>
-            <div class="flex-none">
-              <ul class="menu menu-horizontal px-1">
-                <li><a href="/test1">Test 1</a></li>
-                <li><a href="/test2">Test 2</a></li>
-              </ul>
+                <ul class="menu dropdown-content shadow bg-base-300 rounded-box right-0 p-2 flex-auto min-w-max">
+                    <li><a href="/test1">Test 1</a></li>
+                    <li><a href="/test2">Test 2</a></li>
+                </ul>
             </div>
           </nav>
     </header>
@@ -22,12 +34,10 @@
         <slot />
     </main>
 
-    <footer>App by nightly5</footer>
 </div>
 
 <style lang="postcss">
     @tailwind base;
     @tailwind components;
     @tailwind utilities;
-
 </style>
