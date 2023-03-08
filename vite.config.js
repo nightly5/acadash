ee003d34d1e3be4d88cb3da0752bb05ba10447dd
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-08 13:46:29 +0530

    🔧 do not clear my screen, vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
+    clearScreen: false,
     plugins: [ sveltekit() ],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
-};
-
-export default config;
+});
