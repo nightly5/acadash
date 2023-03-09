9d8eb225e76d911faa66d50dc1822504862192f4
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-10 00:42:09 +0530

    🐛 Dark mode not applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
     daisyui: {
-        themes: ["cmyk", "business"]
+        themes: ["cmyk", "business"],
+        darkTheme: "business"
     },
     content: ["./src/**/*.{html,js,svelte}"],
     plugins: [daisyui]
