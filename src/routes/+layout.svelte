8eceb9ad46176942a592c279e7bc81b6fdcbb329
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-03-10 17:20:39 +0530

    ⚡Better accessibility and styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
     export let data: LayoutData;
 </script>
 
-<div class="app font-sans">
+<div class="app font-sans flex flex-col min-h-screen justify-between">
+    <a href="#main" class="link-base-content left-0 absolute p-3 m-3 mt-16
+    -translate-y-28 focus:translate-y-0">Skip to main content</a>
     <header>
         <nav class="navbar bg-base-300">
             <div class="navbar-start flex-1">
@@ -14,8 +16,8 @@
             </div>
             <div class="navbar-end">
                 <a class="
-                btn btn-ghost font-bold rounded px-8
-                shadow-lg uppercase tracking-wide py-4"
+                btn btn-ghost font-bold rounded px-8 py-4 uppercase
+                tracking-wide"
                 href={data.signed_in ? "/signout": "/auth"}>
                     {#if data.signed_in}Sign out{:else}Sign in{/if}
                 </a>
@@ -23,8 +25,26 @@
           </nav>
     </header>
 
-    <main>
+    <main id="main">
         <slot />
     </main>
+
+    <footer class="footer footer-center link-secondary p-4">
+        <div><div>
+            ·
+                <span role=img aria-label="Written by">✍️</span>
+                <a href="https://github.com/nightly5">nightly5</a>
+            ·
+                <span aria-hidden=true>📝</span>
+                <a href="https://github.com/nightly5/acadash">Code</a>
+            ·
+                <span role=img aria-label="Licensed under">📄</span>
+                <a href="https://github.com/nightly5/LICENSE">BSD-3-Clause</a>
+            ·
+                <span role=img aria-label="Powered by">🚀</span>
+                <a href="https://pages.cloudflare.com">CloudFlare</a>
+            ·
+        </div></div>
+    </footer>
 
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,22 @@
     <meta name="description" content="A lightning-fast academic dashboard" />
 </svelte:head>
 
-<section class="grid place-items-center h-[90vh]">
-    <div class="py-20">
-        <h2 class="text-4xl mb-2 font-bold">
-            Lightning-fast academic dashboard 🚀
-        </h2>
-        <h3 class="text-2xl mb-8">
-            As fast and powerful as a 🐎
-        </h3>
-        <a class="
-        btn btn-accent font-bold text-accent-content
-        rounded-full px-8 py-4 shadow-lg uppercase tracking-wide mb-8"
-        href="/dashboard">
-            Open Dashboard
-        </a>
-    </div>
-    <div class="text-md font-bold">
-        An app by nightly5
+<section class="hero">
+    <div class="hero-content">
+        <div class="max-w-2xl">
+            <div class="text-4xl mb-2 font-bold">
+                Lightning-fast academic dashboard <span aria-hidden=true>⚡</span>
+            </div>
+            <div class="text-2xl mb-8">
+                As fast and powerful as a 🐎
+            </div>
+            <a class="
+            btn btn-accent font-bold text-accent-content
+            rounded-full px-8 py-4 shadow-lg uppercase tracking-wide mb-8"
+            href="/dashboard">
+                Open Dashboard
+            </a>
+        </div>
     </div>
 </section>
 
